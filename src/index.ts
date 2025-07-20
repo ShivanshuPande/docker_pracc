4b3fe43fb4e6d68f2d5eb07208a378d0c301fe86
Shivanshu Pandey <pandeyshivanshu2005@gmail.com>
2025-07-20 15:45:23 +0530

    added the basic route for the app

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,23 @@
-import express from "express";
-const app = express();
+import { PrismaClient } from "@prisma/client";
+import express from "express"
+const app = express;
 app.use(express.json());
 
 const client = new PrismaClient();
 
 
-app.use("/" , (req , res)=>{
+app.get("/" , (req , res)=>{
     res.json({
         message : "This is a healthy server , if you think so its not, its still a pretty healthy server"
     })
+})
+
+
+app.post("/" ,async (req ,res)=>{
+    await client.user.create({
+        data : {
+            name  : req.body.name
+        }
+    })
+
 })
